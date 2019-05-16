0c485d08f999f874e39868b9122ddb1b03634a57
ayuhuajin <455493143@qq.com>
2019-05-16 22:43:30 +0800

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+const Koa = require('koa');
+const statics = require('koa-static');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('koa-bodyparser');
+const router = require('koa-router')();
+const app = new Koa();
+
+// 静态资源目录对于相对入口文件app.js的路径
+const staticPath = './static';
+// 访问路径为 http://localhost:3000/common.css    省略 static
+app.use(statics(
+  path.join( __dirname,  staticPath)
+));
+// parse request body:
+app.use(bodyParser());  //bodypaser要在router之前加载才能生效。
+
+router.get('/index',async(ctx,next)=>{
+  ctx.body='inde4555x';
+  next();
+});
+
+app.use( async(ctx,next) => {
+  console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+  await next();
+});
+app.use(router.routes());   /*启动路由*/
+app.use(router.allowedMethods());
+
+var db =mongoose.connect('mongodb://127.0.0.1:27017/hai_cms');
+console.log(db);
+mongoose.connection.on('error', function (error) {
+  console.log('数据库连接失败：' + error);
+});
+mongoose.connection.on('open', function () {
+  console.log('------数据库连接成功111111！------');
+});
+app.listen(3000);
+console.log('[demo] start-quick is starting at port 3000');
