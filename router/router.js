70336e3d7686c997e46832915e2ab0e686399162
wqh <wqh@nxin.com>
2019-06-02 12:37:09 +0800

    附件上传

--- router/router.js
+++ router/router.js
@@ -1,5 +1,8 @@
 const router = require('koa-router')();
 const Control = require('../controller/control');
+const koaBody = require('koa-body');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = (app) =>{
 
@@ -11,6 +14,20 @@ module.exports = (app) =>{
     // ctx.set('Access-Control-Allow-Methods', 'PUT,DELETE,POST,GET');
     await next();
   });
+  //koaBody   配置
+  app.use(koaBody({
+    multipart: true, // 支持文件上传
+    encoding: 'gzip',
+    formidable: {
+      uploadDir: path.join(__dirname, '../static'), // 设置文件上传目录
+      keepExtensions: true, // 保持文件的后缀
+      maxFieldsSize: 2 * 1024 * 1024, // 文件上传大小
+      onFileBegin: (name, file) => { // 文件上传前的设置
+        console.log(`name: ${name}`);
+        console.log(file);
+      },
+    }
+  }));
 
   // 获取GET请求数据源头
   // http://localhost:12306/index/111/222?category=1&title=2
@@ -21,6 +38,33 @@ module.exports = (app) =>{
     ctx.body='index';
     await next();
   });
+
+  // 上传文件接口;
+  router.post('/upload', async(ctx) => {
+    const file = ctx.request.files.file; // 上传的文件在ctx.request.files.file
+    // 创建可读流
+    const reader = fs.createReadStream(file.path);
+    // 修改文件的名称
+    // var myDate = new Date();
+    // var newFilename = file.name.split('.')[0] + '_' + myDate.getTime() + '.' + file.name.split('.')[1];
+    var targetPath = path.join(__dirname, '../static') + `/${file.name}`;
+    // if (!fs.existsSync(targetPath)) { // 检查是否有“public/upload/”文件夹
+    //   fs.mkdirSync(targetPath); // 没有就创建
+    // }
+    //创建可写流
+    const upStream = fs.createWriteStream(targetPath);
+    // 可读流通过管道写入可写流
+    reader.pipe(upStream);
+    //ctx.redirect('/')
+    //返回保存的路径
+    return ctx.body = {
+      code: 200,
+      data: {
+        url: 'http://' + ctx.headers.host  + file.name
+      }
+    };
+  });
+
   router.get('/search', Control.search);
   // ****************************  分类,增,删,改，查  **********************************//
   router.get('/categoryList', Control.categoryList);
