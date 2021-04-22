8a42c31b15a344ab1f76ee8d6a4d46a28b311930
wangqinghai <455493143@qq.com>
2021-04-22 16:10:38 +0800

    fix:支付宝支付

--- controller/alipay.js
+++ controller/alipay.js
@@ -1,18 +1,25 @@
 // const Axios = require('axios');
 const blog = require('../mongo/blog');
 const AlipaySdk = require('alipay-sdk').default;
+const AlipayFormData = require('alipay-sdk/lib/form').default;
 // const fs = require('fs');
 const alipaySdk = new AlipaySdk({
   appId: '2016102400749295',
+  gateway: 'https://openapi.alipaydev.com/gateway.do',
   // privateKey: fs.readFileSync('../config/private-key.pem', 'ascii'),
   privateKey: `-----BEGIN RSA PRIVATE KEY-----
   MIIEoQIBAAKCAQEAs33ngvlX9xYvKuqq9gTj658CNFF/ydwhdrzb0UlA/3DFq6PBei3I0GGmLYeYTQM9JoCG1cT8LCQ0J1iOlUpspCA4LOBMY1hMWXzn0wF7FaVwneL9Ko3x6rxRA8m/i3yZYVpPgHX0luWSQLkwgAl/frPTCilL3fl+73EaylyCCXzPNfkeOJoj1YbRVPpwBhORPpPeDHVMRo/nz++QgP3OsWHrQOI/fMSjULzI+saGfXhcreuP2DOl2VBDvLHuZgxfdRT5ndr94JNxQeLHIII5tTrr/eBCgv8t31QpPcUfvK8XoctycYKQ/63tcY3l7LfbF1mMEjSRMLQk+tSl0Gwn0QIDAQABAoIBAGkmdA71dcPpeh5hNWBEwyUQlOEDcaJW1D/UKc50ZKCAdzyzBTBHTCJPYmO/0MglApjgqJzo9nYBi5AVQApTfEu2GFKp3/Y/DxNJqiGh93JZ5hyIkplwaG2Efs2CTjUv/Ebc+kAeWfhgawqBstj/N3KJdDO6t5hFhkhNcU1lXkShnI2alen3wyq9InYbYoNoJbIKU2MTsIB/UwaDDXUH6MuOQOGl0ngtvcXHeupzYKDkm0EOl8YVMaNF11GiJ2xo+yOPK+lJ5lXdTLCDb8EWws5TPDUeu9pQu11iHdcCpr66fV8uwWLMTRPmZV3OORfum1csBHRe+RvXfdD8VClciHECgYEA9+cQ+/0GiEbPdy9nIOf/e2NkNyVcvZTaO56osOGG86kTOWmVHCfMe/vwhcD3P2wWoNg8Twjpkvmsz23DJnHAqY/e9WPdmVeHDbW6tC2pd+X0oO8Xc6WvvU/FmozlXQlPGX5Ts7VuL6RjqgWVxe3YuQuAWchm2COazM9fqJg9QU0CgYEAuVrLcDkm0fvruGi1bKoNiyLmKZINoa0oGnQkUGjaFN3W3rQEUToYrFhpA2Gdb0X/GEKtxiZHXJxPSB37DLJGO4eJ3KtpaILPR9tllVy6r4TjLaFI6xNzGgYzswevxf+uPw8px8eA97PiEbs59t33tYBVfP4ey2AwrXgCjdCGvpUCfwe/MMivH4UisVkIIjDlDxbMBFW01TEXdbpnOt5MLetvXSpuIjq/A6TcOqfzX8abmhlsqHXP0XzGUKAqze5ln5+s30rL1CD2m0HSbAM1ahZVGU/uKM10YAmKdKJfxMsjf1VMnVEAWnEisfqeMg/9Xshpk5k6qfwXIL9Fu7eJCmECgYAJmKLJt5vD6VgF3x3td0xmNLNX2aDtCur1m6e5pgQMaoPSWd2TWv+mBJnDPA2LkkMnvWQDyz2MlOch47N4r84lDNpdW16Es6kJnlk/B2L+7VFZrmz5BrPB2CpbDX3TMW6TAzT1i+WobV657kiM+Gb3taI3a2yquNssT/dJ4k7nlQKBgQDMbnoc1KCb520DLQ90n4yfU6WhKpLBGsAvVoD8w6Hgsl+I9CFGdzvsaz/Okd22rw8U094SCwRLSgQLoQkJ4c3T79VLBtUSzEFiUbuYqpTvbAaCwbNTI3dAbb77kJOFa/sL111fr2X8ncSgcbG2EiNFi4F6Hz44VOqNcAx5oGbjSA==
   -----END RSA PRIVATE KEY-----`,
   signType:'RSA2',
   alipayPublicKey:`-----BEGIN PUBLIC KEY-----
-  MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAs33ngvlX9xYvKuqq9gTj658CNFF/ydwhdrzb0UlA/3DFq6PBei3I0GGmLYeYTQM9JoCG1cT8LCQ0J1iOlUpspCA4LOBMY1hMWXzn0wF7FaVwneL9Ko3x6rxRA8m/i3yZYVpPgHX0luWSQLkwgAl/frPTCilL3fl+73EaylyCCXzPNfkeOJoj1YbRVPpwBhORPpPeDHVMRo/nz++QgP3OsWHrQOI/fMSjULzI+saGfXhcreuP2DOl2VBDvLHuZgxfdRT5ndr94JNxQeLHIII5tTrr/eBCgv8t31QpPcUfvK8XoctycYKQ/63tcY3l7LfbF1mMEjSRMLQk+tSl0Gwn0QIDAQAB
+  MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAkQsBcDJY3F0v/C7m5BXRREhrJgOvDNOlu7xY0+iFAPdxQtPebDTDGQYimiVXoCvOWyKZfoHEgBiAeUxq1GWphobID4EkkIo+Bv8XELmp1bdHZxQG+M0j+kgmW9KuJ/yAJM97B5ykSKzmdr90U9c8fGx1t0D9Dl2pT0SjPcqUb875qdcA9s5m+GRpHNFQ8WOmmBY+YoEiAM+S95V9rsAu6VFyNtY0ni/E5KCEAcApJPAy5oB1BF6vEqKElkyAvFoy6MSL5YE4Sal/M9x9AbS+9FBkK5oUaIpwkbdG412AaQnJjfuPN7I9t0cVLMYWU9YWimEzkTgD/8u8nuKM3WSeNwIDAQAB
   -----END PUBLIC KEY-----`,
 });
+const formData = new AlipayFormData();
+formData.setMethod('post');
+
+
+console.log(formData)
 
 
 
@@ -20,23 +27,25 @@ module.exports = {
   createOrder:async(ctx)=>{
     let {out_trade_no,subject,total_amount} = ctx.request.body;
     console.log(4446556,out_trade_no,subject,total_amount);
+    formData.addField('bizContent', {
+      outTradeNo: out_trade_no, // 订单号
+      productCode: 'QUICK_WAP_WAY',
+      totalAmount: '0.01',
+      subject: 'biaoti标题',
+      body: 'miaoshu描述',
+    });
+    // formData.bizContent.outTradeNo = out_trade_no
+    // formData.bizContent.out_trade_no = out_trade_no
+    // formData.bizContent.totalAmount = total_amount
     try{
       // alipay.trade.wap.pay
-      const result = await alipaySdk.exec('alipay.trade.wap.pay',{
-        appId: '2016102400749295',
-        return_url:'https://www.baidu.com',
-        notify_url:'https://api.wulilang.com/orderSuccess',
-        alipayPublicKey:`-----BEGIN PUBLIC KEY-----
-        MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAs33ngvlX9xYvKuqq9gTj658CNFF/ydwhdrzb0UlA/3DFq6PBei3I0GGmLYeYTQM9JoCG1cT8LCQ0J1iOlUpspCA4LOBMY1hMWXzn0wF7FaVwneL9Ko3x6rxRA8m/i3yZYVpPgHX0luWSQLkwgAl/frPTCilL3fl+73EaylyCCXzPNfkeOJoj1YbRVPpwBhORPpPeDHVMRo/nz++QgP3OsWHrQOI/fMSjULzI+saGfXhcreuP2DOl2VBDvLHuZgxfdRT5ndr94JNxQeLHIII5tTrr/eBCgv8t31QpPcUfvK8XoctycYKQ/63tcY3l7LfbF1mMEjSRMLQk+tSl0Gwn0QIDAQAB
-        -----END PUBLIC KEY-----`,
-        bizContent:{
-          out_trade_no: out_trade_no,// 必填 商户订单主键, 就是你要生成的
-          subject: subject,      // 必填 商品概要
-          total_amount: total_amount,    // 必填 多少钱
-          quit_url:'https://www.baidu.com',
-          product_code:'QUICK_WAP_PAY'
-        }
-      });
+      console.log(formData,888);
+      
+      const result = await alipaySdk.exec('alipay.trade.wap.pay', {}, {
+        formData: formData
+      }, { validateSign: true });
+      // console.log(result,8889999);
+      
       ctx.body=result;
     } catch(err) {
       console.log(err,'失败');
